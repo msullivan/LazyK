cda08795fb0308add3313364590ada80a15c575a
Michael Sullivan <sully@msully.net>
2012-03-30 13:50:39 -0400

    Halve the number of requests we make to tweet.

--- twittering/login.sh
+++ twittering/login.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget -U "Mozilla/5.0 (Android; Linux armv7l; rv:5.0) Gecko/20110615 Firefox/5.0 Fennec/5.0" --keep-session-cookies --save-cookies cookies_$1.txt wget -O /dev/null -q --referer=https://mobile.twitter.com/session/new --post-data "username=sttm$1&password=buttsquid" https://mobile.twitter.com/session
+wget -U "Mozilla/5.0 (Android; Linux armv7l; rv:5.0) Gecko/20110615 Firefox/5.0 Fennec/5.0" --keep-session-cookies --save-cookies cookies_$1.txt wget -O - -q --referer=https://mobile.twitter.com/session/new --post-data "username=sttm$1&password=buttsquid" https://mobile.twitter.com/session | grep new_tweet | cut -d'"' -f16 > auth_token_$1.txt
