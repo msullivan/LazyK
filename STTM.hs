420d52e7aadf71040a35ae4ddf66af1655d9a26e
Michael Sullivan <sully@msully.net>
2012-03-29 23:39:47 -0400

    A bunch of hacking and refactoring.

--- STTM.hs
+++ STTM.hs
@@ -11,59 +11,96 @@ import Data.List
 import System.Exit
 import System(getArgs)
 import Control.Monad.State
-import qualified Data.IntMap as M
+import qualified Data.Map as M
 import Data.Maybe
 
 import Web.Codec.URLEncoder
 import Web.Twitter hiding (tweet)
 --import Web.Twitter.Post
 import Web.Twitter.Fetch
-import Web.Twitter.Monad
+import Web.Twitter.Monad hiding (liftIO)
 import Web.Twitter.Types
 
 import System.Process
 
-type Id = Int
-data Atom = S | K | I | Inc | Num Int | Thunk Int
-          deriving (Show, Ord, Eq)
+type Id = String
+data Atom = S | K | I | Inc | Num Int | Thunk Id
+          deriving (Ord, Eq)
+
 type Spine = [Atom]
 type Thunk = (Spine, Bool)
 
+instance Show Atom where
+  show S = "S"
+  show K = "K"
+  show I = "I"
+  show Inc = "+"
+  show (Num i) = show i
+  show (Thunk i) = "#t" ++ i
+
+showUpdate :: Id -> Thunk -> String
+showUpdate id (spine, updated) =
+  "#" ++ op ++ id ++ " " ++ intercalate " " (map show spine) ++ " #sttm"
+  where op = if updated then "u" else "n"
+
 class Monad m => TweetMachine m where
   nameThunk :: Spine -> m Id
-  lookupThunk :: Id -> m Thunk
-  updateThunk :: Id -> Thunk -> m ()
+  publishThunk :: Id -> Thunk -> m ()
   pullThunk :: Id -> m Spine
 
-type LTM = State (Id, M.IntMap Thunk)
+type LTM = State (Int, M.Map Id Thunk)
 instance TweetMachine LTM where
   nameThunk spine = do
     (n, m) <- get
     put (n+1, m)
-    return n
+    return (show n)
 
-  lookupThunk id = do
-    (n, m) <- get
-    return ((M.!) m id)
-
-  updateThunk id thunk = do
+  publishThunk id thunk = do
     (n, m) <- get
     let m' = M.insert id thunk m
     put (n, m')
 
   pullThunk id = do
-    (spine, evaluated) <- lookupThunk id
+    (n, m) <- get
+    let (spine, evaluated) = ((M.!) m id)
     if evaluated then return spine
       else do
       spine' <- evalSpine spine
-      updateThunk id (spine', True)
+      updateThunk id spine'
       return spine'
 
+
+type STTM = StateT (Int, M.Map Id Thunk) IO
+instance TweetMachine STTM where
+  nameThunk spine = do
+    (n, m) <- get
+    put (n+1, m)
+    return (show n)
+
+  publishThunk id thunk = do
+    (n, m) <- get
+    let m' = M.insert id thunk m
+    liftIO $ putStrLn (showUpdate id thunk)
+    --liftIO $ tweet "0001" (showUpdate id thunk)
+    put (n, m')
+
+  pullThunk id = do
+    (n, m) <- get
+    let (spine, evaluated) = ((M.!) m id)
+    if evaluated then return spine
+      else do
+      spine' <- evalSpine spine
+      updateThunk id spine'
+      return spine'
+
+
+updateThunk :: TweetMachine m => Id -> Spine -> m ()
+updateThunk id spine = publishThunk id (spine, isWHNF spine)
 
 newThunk :: TweetMachine m => Spine -> m Id
 newThunk spine = do
   id <- nameThunk spine
-  updateThunk id (spine, False)
+  updateThunk id spine
   return id
 
 spineify :: K.Expr -> [K.Expr]
@@ -93,12 +130,22 @@ stepSpine (Thunk id:es) = do
   return $ spine ++ es
 stepSpine spine = return spine
 
+isWHNF :: Spine -> Bool
+isWHNF (I:e:es) = False
+isWHNF (K:e1:e2:es) = False
+isWHNF (S:e1:e2:e3:es) = False
+isWHNF (Inc:e:es) = False
+isWHNF (Num n:Inc:es) = False
+isWHNF (Thunk id:es) = False
+isWHNF spine = True
+
+
 evalSpine :: TweetMachine m => Spine -> m Spine
 evalSpine spine = do
   spine' <- stepSpine spine
-  if spine == spine' then return spine' else evalSpine spine'
+  if isWHNF spine' then return spine' else evalSpine spine'
 
-flattener :: K.Expr -> (Atom, M.IntMap Thunk)
+flattener :: K.Expr -> (Atom, M.Map Id Thunk)
 flattener e = 
   let (atom, (n, map)) = runState (flattenExpr e) (0 :: Int, M.empty)
   in (atom, map)
@@ -119,11 +166,16 @@ runCombProgram comb = do
 evalCombNumber :: TweetMachine m => K.Expr -> m Int
 evalCombNumber comb = do
   root <- flattenExpr comb
-  [Num n] <- evalSpine [root, Inc, Num 0]
+  id <- newThunk [root, Inc, Num 0]
+  [Num n] <- pullThunk id
   return n
 
 runLTM :: LTM a -> a
 runLTM m = evalState m (0, M.empty)
+
+runSTTM :: STTM a -> IO a
+runSTTM m = evalStateT m (0, M.empty)
+
 
 twitterLogin :: String -> IO ExitCode
 twitterLogin user =
@@ -139,9 +191,9 @@ userSearch user = do
   result <- runTweet (getUserTimeline (Just user) Nothing Nothing)
   return $ map statusText result
 
-main :: IO ()
-main = do
-  --twitterLogin "0001"
+main2 :: IO ()
+main2 = do
+  twitterLogin "0001"
   --tweet "0001" "argh testing! #sttm"
   --let search_ctx = searchFor { searchHashTag = "sttm" }
   --result <- runTweet (search search_ctx)
@@ -151,10 +203,11 @@ main = do
   return ()
 
 
-main2 = do
+main = do
   [sourcePath] <- getArgs
+  --twitterLogin "0001"
   source <- readFile sourcePath
   let comb = K.parse source
   --mapM_ K.outputCharacter $ runLTM (runCombProgram comb)
-  putStrLn (show (runLTM $ evalCombNumber comb))
-  
+  answer <- runSTTM $ evalCombNumber comb
+  putStrLn (show answer)
